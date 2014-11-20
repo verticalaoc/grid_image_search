68c4f3889f2ab89f4cae96dc38fa298392123766
tonydt <tonydt@yahoo-inc.com>
2014-11-21 00:51:59 +0800

    add layout

--- gen/com/codepath/gird_image_search/R.java
+++ gen/com/codepath/gird_image_search/R.java
@@ -25,7 +25,10 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080003;
+        public static final int btnSearch=0x7f080001;
+        public static final int etInput=0x7f080000;
+        public static final int gridView1=0x7f080002;
     }
     public static final class layout {
         public static final int activity_search=0x7f030000;
@@ -37,6 +40,8 @@ public final class R {
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050001;
+        public static final int hint_input=0x7f050003;
+        public static final int label_search=0x7f050004;
     }
     public static final class style {
         /** 
